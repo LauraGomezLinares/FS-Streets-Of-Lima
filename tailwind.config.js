e3a682fdbe8a40c6540f7306ac7bbcf1a9a67d20
LauraGómez <lydiaberry2003@gmail.com>
2026-05-11 01:27:54 -0500

    home done
    pls

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
     extend: {
       fontFamily: {
         dogica: ['Dogica', 'sans-serif'],
+        karmaticarcade: ['Karmatic-Arcade', 'cursive'],
+      },
+      boxShadow: {
+        pixelart: "4px 0 0 0 #64748b, -4px 0 0 0 #64748b, 0 4px 0 0 #64748b, 0 -4px 0 0 #64748b",
       },
     },
   },
