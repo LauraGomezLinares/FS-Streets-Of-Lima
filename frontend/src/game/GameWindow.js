165a4ab4a823a671096e62c1b4f97449a26f77ee
NataliaOspinal <226923708+NataliaOspinal@users.noreply.github.com>
2026-07-06 23:45:51 -0500

    fixing routes

--- frontend/src/game/GameWindow.js
+++ frontend/src/game/GameWindow.js
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 import * as Phaser from "phaser"; 
-import { useLobby } from "../../context/LobbyContext";
-import { useAuth } from "../../context/AuthContext";
+import { useLobby } from "../context/LobbyContext";
+import { useAuth } from "../context/AuthContext";
 import MainScene from "./scenes/MainScene";
 
 export default function GameWindow() {
